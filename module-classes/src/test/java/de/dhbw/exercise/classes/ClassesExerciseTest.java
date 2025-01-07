cb32b15b6da71897c7b1460c35fe4fbc5c3933d8
Thorsten Eckstein <thorsten.eckstein@deutschebahn.com>
2025-01-07 09:58:29 +0100

    basics: added simple module basics for independent student tests

--- module-classes/src/test/java/de/dhbw/exercise/classes/ClassesExerciseTest.java
+++ module-classes/src/test/java/de/dhbw/exercise/classes/ClassesExerciseTest.java
@@ -42,7 +42,7 @@ public class ClassesExerciseTest {
               1. konkrete Klasse `Person` mit einem Attribut `name`, erstelle dann
               2. die zum Attribut gehörende `get` und `set` Methode,
               3. erstelle dann die `equals()` und `hashCode()` Methoden durch Code Generierung,
-              4. zuletzt leite aus dieser konkreten Klasse ein Interface `Mensch` ab
+              4. (optional) leite aus dieser konkreten Klasse ein Interface `Mensch` ab
             """)
     public void exercise3() {
         // given
