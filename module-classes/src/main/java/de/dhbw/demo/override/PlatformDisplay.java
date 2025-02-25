58168150d7c34b28170205beccfe7625d9f6b78e
thorsteneckstein <thorsten_eckstein@web.de>
2025-02-25 10:34:52 +0100

    classes: doc improvements

--- module-classes/src/main/java/de/dhbw/demo/override/PlatformDisplay.java
+++ module-classes/src/main/java/de/dhbw/demo/override/PlatformDisplay.java
@@ -2,6 +2,7 @@ package de.dhbw.demo.override;
 
 public class PlatformDisplay extends DefaultDisplay {
 
+    @Override
     public void update(int index, String row) {
         String shortenedRow = shorten(row);
         super.update(index, shortenedRow);
