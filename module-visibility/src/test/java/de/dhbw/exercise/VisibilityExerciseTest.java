dfb40e815257c1414bfe66ca932ec8ce25f50766
thorsteneckstein <thorsten_eckstein@web.de>
2025-03-05 07:52:36 +0100

    visibility: added exercise

---
+++ module-visibility/src/test/java/de/dhbw/exercise/VisibilityExerciseTest.java
@@ -0,0 +1,29 @@
+package de.dhbw.exercise;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class VisibilityExerciseTest {
+
+    @Test
+    @DisplayName("""
+        Übung 1:
+        a) Implementiere eine Klasse 'Person' mit den Attributen
+        - Name
+        - Alter
+        b) Entscheide selbst über die Datentypen der Attribute, aber mache sie nicht öffentlich.
+        c) Erstelle dazu einen Konstruktor für die Initialisierung aller Attribute.
+        d) Erstelle zudem Methoden für das Auslesen und Setzen der Attributwerte ("getter & setter").
+        
+        Schreibe hier einen einfachen Test zur Überprüfung eines der Felder.
+        """)
+    public void exercise1() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
