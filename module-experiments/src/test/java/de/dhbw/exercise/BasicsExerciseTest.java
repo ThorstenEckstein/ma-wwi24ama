a009d50f743bfe1dd39dd60ac186ee1c67d6bc27
Thorsten Eckstein <thorsten.eckstein@deutschebahn.com>
2025-04-04 11:24:21 +0200

    module renamed: module-basics -> module-experiments

--- module-experiments/src/test/java/de/dhbw/exercise/BasicsExerciseTest.java
+++ module-experiments/src/test/java/de/dhbw/exercise/BasicsExerciseTest.java
@@ -1,19 +1,16 @@
-package de.dhbw.solution;
+package de.dhbw.exercise;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-@SuppressWarnings({"SimplifiableAssertion", "ConstantValue"})
-public class BasicsSolutionTest {
+public class BasicsExerciseTest {
 
     @Test
     @DisplayName(
             """
             Erstelle eine beliebige, fachliche Klasse deiner Wahl im 'Package'
             
-              module-basics/src/main/java/de/dhbw/exercise/???.java
+              module-experiments/src/main/java/de/dhbw/exercise/???.java
             
             in diesem Modul, instanziiere diese hier im Test und prüfe, ob die Instanz existiert.
             Führe diesen Test durch Klick auf das 'Play' Symbol am linken Rand des Editors aus.
@@ -22,12 +19,10 @@ public class BasicsSolutionTest {
             """)
     public void exercise1() {
         // given
-        Lunch lunch = new Lunch();
 
         // when
 
         // then
-        assertFalse(lunch == null);
     }
 
 }
