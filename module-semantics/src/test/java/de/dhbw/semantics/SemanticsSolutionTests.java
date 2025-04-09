ca39a89a1ab0d44d883d3ea24e691108e593e105
thorsteneckstein <thorsten_eckstein@web.de>
2025-04-09 07:57:51 +0200

    day 8: update

--- module-semantics/src/test/java/de/dhbw/semantics/SemanticsSolutionTests.java
+++ module-semantics/src/test/java/de/dhbw/semantics/SemanticsSolutionTests.java
@@ -15,18 +15,19 @@ public class SemanticsSolutionTests {
     @Disabled
     public void demo4() {
         Account a1 = new Account();
-        //assertEquals(-1, a1.getAmount(), "Welcher Wert wird hier erwartet?");
+        //assertEquals(-1, a1.getAmount(), "Welcher Wert wird hier für a1 erwartet?");
 
         Account.process(a1);
 
         Account a2 = new Account(100);
-        //assertEquals(-1, a2.getAmount(), "Welcher Wert wird hier erwartet?");
+        //assertEquals(-1, a2.getAmount(), "Welcher Wert wird hier für a2 erwartet?");
 
         Account acc = new Account();
         acc = a2;
+        //assertEquals(-1, acc.getAmount(), "Welcher Wert wird hier für acc erwartet?");
 
         acc.deposit(250);
-        //assertEquals(-1, acc.getAmount(), "Welcher Wert wird hier erwartet?");
+        //assertEquals(-1, acc.getAmount(), "Welcher Wert wird hier für acc erwartet?");
     }
 
 }
