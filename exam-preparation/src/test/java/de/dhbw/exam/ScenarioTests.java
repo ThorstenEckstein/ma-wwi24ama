68a7aae911e8966fcae6cf715848441f21726901
thorsteneckstein <thorsten_eckstein@web.de>
2025-04-16 12:13:54 +0200

    modell übungen live im kurs, temporär, später löschen

--- exam-preparation/src/test/java/de/dhbw/exam/ScenarioTests.java
+++ exam-preparation/src/test/java/de/dhbw/exam/ScenarioTests.java
@@ -1,11 +1,50 @@
 package de.dhbw.exam;
 
+import de.dhbw.course.Inhalt;
+import de.dhbw.course.Lehrveranstaltung;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScenarioTests {
+
+    @Test
+    public void canRealizeScenarioLV() {
+        // given
+        Lehrveranstaltung prog2 = new Lehrveranstaltung();
+        Inhalt grundlagen = new Inhalt();
+        Inhalt schnittstellen = new Inhalt();
+        Inhalt operatoren = new Inhalt();
+
+        // when
+        grundlagen.setName("Java Grundlagen");
+        grundlagen.setDauer(45);
+        schnittstellen.setName("Java Schnittstellen");
+        schnittstellen.setDauer(30);
+        operatoren.setName("Java Operatoren");
+        operatoren.setDauer(15);
+
+        prog2.setName("Java Programmierung");
+        prog2.setBeginn(LocalTime.of(8,0,0));
+
+        List<Inhalt> inhalte = new ArrayList<>();
+        inhalte.add(grundlagen);
+        inhalte.add(schnittstellen);
+        inhalte.add(operatoren);
+
+        prog2.setInhalte(inhalte);
+        Integer gesamtDauerProg2 = prog2.getGesamtDauer();
+
+        // then
+        assertEquals(3, prog2.getInhalte().size());
+        assertEquals(90, gesamtDauerProg2);
+    }
+
 
     @Test
     @DisplayName("Stromkunden-Verwaltung")
