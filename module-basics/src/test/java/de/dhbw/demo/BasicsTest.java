33d111ef3acc068ba7a9c437839a05c9c37f167b
thorsteneckstein <thorsten_eckstein@web.de>
2025-10-10 14:43:22 +0200

    ss26 preparation, added basics module

---
+++ module-basics/src/test/java/de/dhbw/demo/BasicsTest.java
@@ -0,0 +1,16 @@
+package de.dhbw.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicsTest {
+
+    @Test
+    public void canDemonstrateStatements() {
+        // given
+        int index = 1;
+        int _idx = 1 + 2;
+        String firstName = "Miguel";
+        var isActive = true;
+    }
+
+}
