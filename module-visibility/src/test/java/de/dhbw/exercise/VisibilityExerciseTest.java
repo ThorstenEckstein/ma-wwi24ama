1c2c74c56d0b738c1117e5ed3657b6398981199f
Thorsten Eckstein <thorsten.eckstein@deutschebahn.com>
2025-10-28 16:39:07 +0100

    structure update, day 1,2 and 3

--- module-visibility/src/test/java/de/dhbw/exercise/VisibilityExerciseTest.java
+++ module-visibility/src/test/java/de/dhbw/exercise/VisibilityExerciseTest.java
@@ -9,13 +9,14 @@ public class VisibilityExerciseTest {
     @DisplayName("""
         Übung 1:
         a) Implementiere eine Klasse 'Person' mit den Attributen
-        - Name
-        - Alter
-        b) Entscheide selbst über die Datentypen der Attribute, aber mache sie nicht öffentlich.
-        c) Erstelle dazu einen Konstruktor für die Initialisierung aller Attribute.
-        d) Erstelle zudem Methoden für das Auslesen und Setzen der Attributwerte ("getter & setter").
+         - Name
+         - Alter
+        b) Entscheide selbst über die Datentypen der Attribute, aber mache sie "nicht öffentlich".
+        c) Erstelle zudem Methoden für das Auslesen und Setzen der Attribute (get-/setName, get-/setAge)
+        d) Schreibe hier einen einfachen Test: Setze konkrete Werte für Name und Alter (set), überprüfe dann,
+           ob die Werte korrekt gesetzt wurden (get).
         
-        Schreibe hier einen einfachen Test zur Überprüfung eines der Felder.
+        Achte bewusst auf die Sichtbarkeiten der Attribute und Methoden und erkläre diese!
         """)
     public void exercise1() {
         // given
