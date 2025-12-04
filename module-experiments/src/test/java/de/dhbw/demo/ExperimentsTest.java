a74d3ace39c2a7e3658cb48f943342ee04b9f0ad
Thorsten Eckstein <thorsten.eckstein@deutschebahn.com>
2025-12-04 08:15:47 +0100

    clean up module-experiments

---
+++ module-experiments/src/test/java/de/dhbw/demo/ExperimentsTest.java
@@ -0,0 +1,19 @@
+package de.dhbw.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ExperimentsTest {
+
+    @Test
+    public void demo1() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
