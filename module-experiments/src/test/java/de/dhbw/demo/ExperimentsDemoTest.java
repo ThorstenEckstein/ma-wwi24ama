53cdc8c61a7bbddb7243d955c3e44071c6468c55
Thorsten Eckstein <thorsten.eckstein@deutschebahn.com>
2025-12-04 08:22:22 +0100

    added module 'module-ai' as skeleton, still empty and wip

---
+++ module-experiments/src/test/java/de/dhbw/demo/ExperimentsDemoTest.java
@@ -0,0 +1,19 @@
+package de.dhbw.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ExperimentsDemoTest {
+
+    @Test
+    public void demo1() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
