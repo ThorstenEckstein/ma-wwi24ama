9b7bc7c2ce5bc563ab124a500069d86ecffe4eb1
thorsteneckstein <thorsten_eckstein@web.de>
2026-03-25 14:28:45 +0100

    ss26: LV 7 updates

--- module-exceptions/src/test/java/de/dhbw/exceptions/ExceptionsExerciseTests.java
+++ module-exceptions/src/test/java/de/dhbw/exceptions/ExceptionsExerciseTests.java
@@ -8,10 +8,10 @@ public class ExceptionsExerciseTests {
     @Test
     @DisplayName("""
         Übung 1: Exception werfen und fangen
-        1. Implementiere das Interface Executable mit einer Methode void execute(), dazu eine Klasse Task, die das
+        1. Implementiere ein Interface 'Executable' mit einer Methode 'void execute()', dazu eine Klasse 'Task', die das
            Interface implementiert
-        2. Implementiere dazu auch eine neue konkrete, eigene 'CheckedException', die von Exception abgeleitet werden
-           soll und von execute() geworfen werden kann.
+        2. Implementiere dazu auch eine neue konkrete, eigene 'Exception' (CheckedException), die von Exception
+           abgeleitet werden soll und von 'execute()' geworfen werden kann.
         3. Wirf diese neue Exception einfach mittels throws in der konkreten Methode execute() deiner konkreten Klasse.
         4. Nutze diesen Unit-Test, erzeuge einen Task, rufe die Methode execute() auf und fange die geworfene Exception.
     """)
